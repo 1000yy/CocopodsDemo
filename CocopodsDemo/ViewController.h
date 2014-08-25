8a40066345aaf72c54980f4ce3d8f1b42182df72
suzq <suzq@szqMac.local>
2014-08-25 17:15:23 +0800

    Initial Commit

---
+++ CocopodsDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CocopodsDemo
+//
+//  Created by suzq on 14-8-25.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CocopodsDemoTests/CocopodsDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  CocopodsDemoTests.m
+//  CocopodsDemoTests
+//
+//  Created by suzq on 14-8-25.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocopodsDemoTests : XCTestCase
+
+@end
+
+@implementation CocopodsDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
